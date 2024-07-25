467837ffca16f78f565a83d1fbb52bf4df6ad4db
Tuan Kiet Tran <charlieismylife2003@gmail.com>
2024-07-25 23:08:06 +0700

    fix: remove file logging

--- src/logger/logger.module.ts
+++ src/logger/logger.module.ts
@@ -1,17 +1,11 @@
 import { Module } from '@nestjs/common';
 import { LoggerModule as PinoLoggerModule } from 'nestjs-pino';
-import * as pino from 'pino';
 
 @Module({
   imports: [
     PinoLoggerModule.forRoot({
       pinoHttp: {
-        transport: process.env.NODE_ENV !== 'production' ? { target: 'pino-pretty' } : undefined,
-        stream: pino.destination({
-          dest: `logs/${new Date().toISOString().split('T')[0]}.log`,
-          sync: false,
-          mkdir: true
-        })
+        transport: process.env.NODE_ENV !== 'production' ? { target: 'pino-pretty' } : undefined
       }
     })
   ]

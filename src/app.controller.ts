252d5f6566a72024126f901c15654b0299aeb232
Tuan Kiet Tran <charlieismylife2003@gmail.com>
2024-07-25 22:21:45 +0700

    chore: add postman environment changing

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { Public } from './shared/decorators';
 
 @Controller()
-export class AppController {}
+export class AppController {
+  @Public()
+  @Get()
+  changePostmanEnvironment(@Query('env') env: string) {
+    return env === 'production' ? 'prod' : 'dev';
+  }
+}
